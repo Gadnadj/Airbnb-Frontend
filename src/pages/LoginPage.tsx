a7d1ab072a7a9fa78a4cee979c65972b4069bd93
Gad Nadjar <gadnadjar@gmail.com>
2025-01-06 15:18:31 +0200

    add some css styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,7 +4,14 @@ type Props = {}
 
 const LoginPage = (props: Props) => {
     return (
-        <div>LoginPage</div>
+        <div className='mt-4'>
+            <h1 className='text-4xl text-center'>Login</h1>
+            <form className='max-w-md mx-auto'>
+                <input type="email" placeholder='your@email.com' />
+                <input type="password" placeholder='password' />
+                <button className='primary'>Login</button>
+            </form>
+        </div>
     );
 };
 
