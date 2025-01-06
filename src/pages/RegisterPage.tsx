6eee4f51514b5bfafcae7a7c0f5d123cb7f57770
Gad Nadjar <gadnadjar@gmail.com>
2025-01-06 15:36:25 +0200

    fix register link

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {}
 
 const RegisterPage = (props: Props) => {
     return (
-        <div>RegisterPage</div>
+        <div className='mt-4 grow flex items-center justify-around'>
+            <div className='mb-64'>
+                <h1 className='text-4xl text-center mb-4'>Register</h1>
+                <form className='max-w-md mx-auto'>
+                    <input type="email" placeholder='your@email.com' />
+                    <input type="password" placeholder='password' />
+                    <button className='primary'>Register</button>
+                    <div className='text-center py-2 text-gray-500'>
+                        Have an account? <Link to={'/login'} className='underline text-black hover:text-primary'>Login In</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 };
 
