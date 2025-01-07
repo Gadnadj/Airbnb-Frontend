90581a9cc384da79e6e22ecd56dd3f41bcf4e4b4
Gad Nadjar <gadnadjar@gmail.com>
2025-01-07 17:53:49 +0200

    fix last bug

--- src/Perks.tsx
+++ src/Perks.tsx
@@ -49,9 +49,9 @@ const Perks = ({ selected, onChange }: Props) => {
             </label>
 
             <label className='border p-4 flex rounded-2xl gap-2 items-center cursor-pointer'>
-                <input type="checkbox" name='private' onChange={handleCbClick} />
+                <input type="checkbox" name='entrance' onChange={handleCbClick} />
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="entrance" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
                 </svg>
                 <span>Private Entrance</span>
             </label>
