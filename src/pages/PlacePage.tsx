42a4c9be949aa64975c379a682d574d0812f412f
Gad Nadjar <gadnadjar@gmail.com>
2025-01-12 13:30:05 +0200

    fix bugs

--- src/pages/PlacePage.tsx
+++ src/pages/PlacePage.tsx
@@ -23,11 +23,11 @@ const PlacePage = (props: Props) => {
 
     if (showAllPhotos) {
         return (
-            <div className="absolute inset-0 bg-white min-h-screen">
-                <div className="p-8 grid gap-4">
+            <div className="absolute inset-0 bg-black min-h-screen text-white">
+                <div className="bg-black p-8 grid gap-4">
                     <div>
                         <h2 className="text-3xl">Photos of {place.title}</h2>
-                        <button className="fixed flex items-center gap-1 py-2 px-4 rounded-2xl text-black bg-gray-200 shadow-lg shadow-gray-400 text-lg right-12 top-8">
+                        <button onClick={() => setShowAllPhotos(false)} className="text-gray-700 fixed flex items-center gap-1 py-2 px-4 rounded-2xl text-black bg-gray-200 shadow-lg shadow-gray-400 text-lg right-12 top-8">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                 <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                             </svg>
